d2649ff43a9a14c980ccdf979b5db7a0ec90f16e
Igor Zanella <igorzanella.dev@gmail.com>
2021-09-12 16:38:54 +0200

    feat: continue SEO

--- components/core/SEO/index.tsx
+++ components/core/SEO/index.tsx
@@ -1,31 +1,39 @@
-import {FunctionComponent} from "react";
+import { FunctionComponent } from "react";
 import Head from "next/head";
-import {OpenGraphObject, TwitterCard} from "../../../types/seo";
-import {Thing, WithContext} from "schema-dts";
-import {TITLE_SUFFIX, URL} from "../../../constants/seo";
-import {useRouter} from "next/router";
+import { OpenGraphObject, TwitterCard } from "../../../types/seo";
+import { Thing, WithContext } from "schema-dts";
+import { TITLE_SUFFIX, URL } from "../../../constants/seo";
+import { useRouter } from "next/router";
 
 interface SEOProps {
-    title: string;
-    withoutTitleSuffix?: boolean;
-    withoutDefaultStructuredData?: boolean;
-    description?: string;
-    canonical?: string;
-    image?: string;
-    openGraph?: OpenGraphObject;
-    twitterCard?: TwitterCard;
-    meta?: JSX.IntrinsicElements["meta"][];
-    structuredData?: WithContext<Thing>[];
+  title: string;
+  withoutTitleSuffix?: boolean;
+  withoutDefaultStructuredData?: boolean;
+  description?: string;
+  canonical?: string;
+  image?: string;
+  openGraph?: OpenGraphObject;
+  twitterCard?: TwitterCard;
+  meta?: JSX.IntrinsicElements["meta"][];
+  structuredData?: WithContext<Thing>[];
 }
 
 const SEO: FunctionComponent<SEOProps> = (props: SEOProps) => {
-    return (
-        <Head>
-            <title>{props.title}{!props.withoutTitleSuffix && TITLE_SUFFIX}</title>
-            {props.description && <meta name="description" content={props.description}/>}
-            <link rel="canonical" href={props.canonical ? props.canonical : URL + useRouter().pathname} />
-        </Head>
-    )
-}
+  return (
+    <Head>
+      <title>
+        {props.title}
+        {!props.withoutTitleSuffix ? TITLE_SUFFIX : ""}
+      </title>
+      {props.description && (
+        <meta name="description" content={props.description} />
+      )}
+      <link
+        rel="canonical"
+        href={props.canonical ? props.canonical : URL + useRouter().pathname}
+      />
+    </Head>
+  );
+};
 
 export default SEO;
