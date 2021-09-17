5db4d60876b9f9c60a7b68631ea4c1a6ba56e50a
Igor Zanella <igorzanella.dev@gmail.com>
2021-09-17 02:41:25 +0200

    feat: add iz colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "iz-yellow-dark": "#ff9933",
+        "iz-yellow-light": "#ffcc00",
+        "iz-blue-dark": "#005cb4",
+        "iz-blue-light": "#00ccff",
+      },
+    },
   },
   variants: {
     extend: {},
