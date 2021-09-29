f16f4f2e894769aae738db660abb322c9f13b4d3
Igor Zanella <igorzanella.dev@gmail.com>
2021-09-29 11:08:26 +0200

    feat: add gtm and initialize

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,2 +1,3 @@
 export const TITLE = "igorzanella.dev";
 export const TITLE_SUFFIX = " | " + TITLE;
+export const GTM_ID = "GTM-5MW4L2G";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import { GTM_ID } from "../constants/seo";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: GTM_ID,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
