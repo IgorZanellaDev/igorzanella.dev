d618626d9d4bb822abf56528b16a6ca495e8c016
Igor Zanella <igorzanella.dev@gmail.com>
2021-09-29 11:08:56 +0200

    fix: fix header section

--- components/ui/extended/Layout/Header/Navbar/index.tsx
+++ components/ui/extended/Layout/Header/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import Logo from "../../../../../core/Logo";
-import Icon from "../../../../base/Icon";
+import Icon from "../../../../../core/Icon";
 import NavbarItem from "./NavbarItem";
 import {
   GITHUB_URL,
@@ -11,27 +11,27 @@ import {
 const Navbar: FunctionComponent = () => {
   return (
     <nav
-      className="hidden md:flex justify-between items-center pt-4 mb-12"
+      className="hidden md:grid grid-cols-3 w-full items-center pt-4 mb-12"
       role="navigation"
       aria-label="Main navigation"
     >
       <div className="flex flex-row items-center space-x-10">
-        <NavbarItem href={"/"}>Home</NavbarItem>
+        <NavbarItem href={"/"}>Hello</NavbarItem>
         <NavbarItem href={"/projects"}>Projects</NavbarItem>
         <NavbarItem href={"/about"}>About</NavbarItem>
       </div>
-      <div className="flex h-auto">
+      <div className="flex justify-center items-center h-auto">
         <Logo withoutText />
       </div>
-      <div className="flex justify-center items-center space-x-6">
+      <div className="flex justify-end items-center space-x-6">
         <NavbarItem href={GITHUB_URL}>
-          <Icon size={32} type={"github"} />
+          <Icon size={24} type={"github"} />
         </NavbarItem>
         <NavbarItem href={LINKEDIN_URL}>
-          <Icon size={32} type={"linkedin"} />
+          <Icon size={24} type={"linkedin"} />
         </NavbarItem>
         <NavbarItem href={TWITTER_URL}>
-          <Icon size={32} type={"twitter"} />
+          <Icon size={24} type={"twitter"} />
         </NavbarItem>
       </div>
     </nav>
