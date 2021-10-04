db192ec1f71175494e65ba70a4db9d3c4d1f320a
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-05 00:50:00 +0200

    feat: add sections to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import GlobalWrapper from "../components/core/GlobalWrapper";
 import SEO from "../components/core/SEO";
 import HeaderSection from "../components/ui/extended/HeaderSection";
+import StartProjectCTASection from "../components/ui/extended/StartProjectCTASection";
+import ServicesSection from "../components/ui/extended/ServicesSection";
 
 /**
  * Header
@@ -15,6 +17,11 @@ const Home: NextPage = () => {
     <GlobalWrapper withLayout>
       <SEO title="Hello" />
       <HeaderSection />
+      {/*What do I do? (services divided by dev and devops)*/}
+      <ServicesSection />
+      {/*StartProjectCTASection: Want to start a new project? CTA*/}
+      <StartProjectCTASection />
+      {/*Testimonials*/}
     </GlobalWrapper>
   );
 };
