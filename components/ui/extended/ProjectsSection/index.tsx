09b5b5487eb049b53cabcc9d5aada9db63b4ca54
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-11 00:15:21 +0200

    feat: add ProjectsSection

---
+++ components/ui/extended/ProjectsSection/index.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent } from "react";
+import Project from "components/ui/base/Project";
+
+const ProjectsSection: FunctionComponent = () => {
+  return (
+    <section>
+      <Project
+        id={"bestondesk"}
+        type={"Personal side project"}
+        title={"BestOnDesk"}
+        description={
+          "Italian blog, business created to earn with affiliate marketing, Amazon and others."
+        }
+        imageWidth={1920}
+        imageHeight={1080}
+      />
+      <Project
+        inverted
+        id={"customerly"}
+        type={"Customer project"}
+        title={"Customerly"}
+        description={
+          "Italian blog, business created to earn with affiliate marketing, Amazon and others."
+        }
+        imageWidth={1950}
+        imageHeight={1010}
+      />
+      <Project
+        id={"amz-discount-bot"}
+        type={"Personal side project"}
+        title={"AMZ DiscountBot"}
+        description={
+          "Italian blog, business created to earn with affiliate marketing, Amazon and others."
+        }
+        imageWidth={1920}
+        imageHeight={1080}
+      />
+      <Project
+        inverted
+        id={"racing-line-motorsport"}
+        type={"Team project"}
+        title={"Racing Line Motorsport"}
+        description={
+          "Sim racing international team, competing in most important championships in the world"
+        }
+        imageWidth={1920}
+        imageHeight={1080}
+      />
+    </section>
+  );
+};
+
+export default ProjectsSection;
