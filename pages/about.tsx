c8a86914adcd3b598bb4606a167581b515dcfa1d
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-10 16:07:36 +0200

    feat: start About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import GlobalWrapper from "components/core/GlobalWrapper";
 import SEO from "components/core/SEO";
+import TitleSection from "components/ui/base/TitleSection";
 
 const About: NextPage = () => {
   return (
     <GlobalWrapper withLayout>
       <SEO title="About" />
-      <h1>About</h1>
+      <TitleSection
+        subtitle={"About"}
+        title={"Do you want to get to know me?"}
+        description={"You will discover my previous jobs and my passions"}
+      />
     </GlobalWrapper>
   );
 };
