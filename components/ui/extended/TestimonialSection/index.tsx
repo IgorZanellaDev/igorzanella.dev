dd3c896151a80e174f213c2f5b7b0705f7b236d6
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-10 02:22:02 +0200

    feat: complete TestimonialSection

--- components/ui/extended/TestimonialSection/index.tsx
+++ components/ui/extended/TestimonialSection/index.tsx
@@ -1,11 +1,10 @@
 import React, { FunctionComponent } from "react";
-import Icon from "components/core/Icon";
 
 const TestimonialSection: FunctionComponent = () => {
   return (
     <section className="py-16">
       <div className="relative">
-        <div className="relative py-24 px-8 bg-gradient-to-b from-iz-blue-light to-iz-blue-dark rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
+        <div className="relative py-24 px-8 bg-gradient-to-b from-iz-blue-light to-iz-blue-dark rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:gap-x-8">
           <div className="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
             <img
               src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80"
@@ -13,13 +12,18 @@ const TestimonialSection: FunctionComponent = () => {
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="relative lg:col-span-1">
+          <div className="relative">
             <img className="h-12 w-auto" src="/icons/customerly.svg" alt="" />
             <blockquote className="mt-6 text-white">
-              <p className="text-xl font-medium sm:text-2xl">
-                Workflow has completely transformed how we interact with
-                customers. We've seen record bookings, higher customer
-                satisfaction, and reduced churn.
+              <p className="text-base font-medium sm:text-2xl">
+                Igor is an amazing person to work with. He's talented and
+                passionate about what he does. He's always searching for the
+                best technology to use. He's a continuous learner and listener.
+                He joined the Customerly culture from the very first day and I
+                believe is one of the best developers I've ever known. His
+                creations at Customerly are astonishing and brought immense
+                value to the platform. On the human side, he's a great person
+                with soft skills and moreover a great friend.
               </p>
               <footer className="mt-6">
                 <p className="flex flex-col font-medium">
