df9d95cd133f5c6501ebe378bf58832219b2266e
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-13 02:20:43 +0200

    feat: modify Typography margins

--- components/ui/base/Typography/h2.tsx
+++ components/ui/base/Typography/h2.tsx
@@ -7,7 +7,7 @@ interface H2Props {
 
 const H2: FunctionComponent<H2Props> = (props: H2Props) => {
   return (
-    <h2 className={`text-white text-4xl font-bold mt-2 ${props.className}`}>
+    <h2 className={`text-white text-4xl font-bold mt-4 ${props.className}`}>
       {props.children}
     </h2>
   );
--- components/ui/base/Typography/p.tsx
+++ components/ui/base/Typography/p.tsx
@@ -5,7 +5,7 @@ interface PProps {
 }
 
 const P: FunctionComponent<PProps> = (props: PProps) => {
-  return <p className="mt-4 text-lg text-white">{props.children}</p>;
+  return <p className="mt-3 text-lg text-white">{props.children}</p>;
 };
 
 export default P;
