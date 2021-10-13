e51c4c34ebc7abec2dc5d1a61cf920c0d91bb34a
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-13 14:57:51 +0200

    feat: add responsive behaviour to ProjectTitle

--- components/ui/base/ProjectTitle/index.tsx
+++ components/ui/base/ProjectTitle/index.tsx
@@ -25,18 +25,18 @@ const ProjectTitle: FunctionComponent<ProjectTitleProps> = (
 ) => {
   return (
     <section className="mb-8">
-      <div className="flex mx-auto py-6 space-x-8">
-        <div className="w-1/2">
+      <div className="flex flex-col lg:flex-row mx-auto py-6 space-y-6 lg:space-y-0 lg:space-x-8">
+        <div className="w-full lg:w-1/2">
           <p className="text-base font-semibold text-iz-blue-light tracking-wide uppercase">
             {props.subtitle}
           </p>
           <h1 className="mt-1 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
             {props.name}
           </h1>
-          <p className="mt-2 text-3xl font-semibold tracking-wide uppercase text-transparent bg-clip-text bg-gradient-to-b from-iz-yellow-light to-iz-yellow-dark">
+          <p className="mt-2 text-xl sm:text-xl lg:text-3xl font-semibold tracking-wide uppercase text-transparent bg-clip-text bg-gradient-to-b from-iz-yellow-light to-iz-yellow-dark">
             {props.category}
           </p>
-          <div className="flex mt-6 space-x-4">
+          <div className="flex mt-6 space-x-2 md:space-x-3 lg:space-x-4">
             {props.links.web && (
               <LinkIcon icon={"globe"} href={props.links.web} />
             )}
@@ -50,10 +50,10 @@ const ProjectTitle: FunctionComponent<ProjectTitleProps> = (
               <LinkIcon icon={"twitter"} href={props.links.twitter} />
             )}
           </div>
-          <h2 className="mt-6 text-3xl font-semibold text-white sm:text-5xl sm:tracking-tight lg:text-4xl">
+          <h2 className="mt-6 text-2xl md:text-3xl font-semibold text-white sm:tracking-tight lg:text-4xl">
             Technologies
           </h2>
-          <div className="flex justify-center lg:justify-start items-center flex-row flex-wrap -mx-4 mt-2">
+          <div className="flex items-center flex-row flex-wrap -mx-4 mt-2">
             {exportTechnologies(props.technologies).map((technology, i) => (
               <TechnologyElement
                 technology={technology}
@@ -63,11 +63,11 @@ const ProjectTitle: FunctionComponent<ProjectTitleProps> = (
             ))}
           </div>
         </div>
-        <div className="w-1/2 relative">
+        <div className="w-full h-64 sm:h-96 lg:h-auto lg:w-1/2 relative">
           <Image
             src={`/projects/${props.id}/main.jpg`}
             layout="fill"
-            className="overflow-hidden rounded-2xl object-cover"
+            className="overflow-hidden rounded-lg md:rounded-xl lg:rounded-2xl object-cover"
           />
         </div>
       </div>
