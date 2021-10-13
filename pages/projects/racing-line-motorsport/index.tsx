11089406a744f552d9df2ef127f585f3af5b6a6f
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-13 02:22:04 +0200

    feat: continue RLM project

--- pages/projects/racing-line-motorsport/index.tsx
+++ pages/projects/racing-line-motorsport/index.tsx
@@ -6,6 +6,8 @@ import ProjectTitle from "components/ui/base/ProjectTitle";
 import H2 from "components/ui/base/Typography/h2";
 import P from "components/ui/base/Typography/p";
 import ProjectSection from "components/ui/base/ProjectSection";
+import A from "components/ui/base/Typography/a";
+import H3 from "components/ui/base/Typography/h3";
 
 const RacingLineMotorsport: NextPage = () => {
   return (
@@ -13,6 +15,12 @@ const RacingLineMotorsport: NextPage = () => {
       <SEO title={"Racing Line Motorsport"} />
       <ProjectTitle
         category={"Sim Racing"}
+        links={{
+          web: "https://racinglinemotorsport.com",
+          instagram: "https://www.instagram.com/racinglinemotorsport/",
+          facebook: "https://www.facebook.com/RacingLineMotorsport/",
+          twitter: "https://twitter.com/racingliners",
+        }}
         id={"racing-line-motorsport"}
         name={"Racing Line Motorsport"}
         subtitle={"Team project"}
@@ -33,6 +41,7 @@ const RacingLineMotorsport: NextPage = () => {
           "nodejs",
           "react",
           "redux",
+          "serverless",
           "windows",
         ]}
       />
@@ -48,8 +57,46 @@ const RacingLineMotorsport: NextPage = () => {
           beginning. Competing against other teams like Ferrari and working with
           real automotive brands like Bentley and Mercedes AMG.
         </P>
-        <H2>React website</H2>
-        <P>Racing Line Motorsport also organize races, which are </P>
+        <H2>Official website</H2>
+        <P>
+          Racing Line Motorsport also organize races, which are available on the{" "}
+          <A
+            href={"https://racinglinemotorsport.com"}
+            target="_blank"
+            rel="noopener"
+          >
+            official website
+          </A>
+          . Website has been created also to show to the world the team members
+          and to share contacts and community links.
+        </P>
+        <H3>Frontend</H3>
+        <P>
+          I completely created the frontend by my own, it's based on React, with
+          2 available languages, I used Bootstrap as CSS Framework, then there
+          is Redux for state management and i18next. It's CI-built on Netlify.
+          We use Google Analytics for visitors analysis.
+        </P>
+        <H3>Backend</H3>
+        <P>
+          Backend has been made by a friend of mine, I helped him sometimes.
+          Backend is on Node.JS and Express, on MongoDB, it's hosted on AWS,
+          with Serverless framework.
+        </P>
+        <H3>News section</H3>
+        <P>
+          After website I also created a separate{" "}
+          <A
+            href={"https://news.racinglinemotorsport.com/"}
+            target="_blank"
+            rel="noopener"
+          >
+            news section
+          </A>{" "}
+          based on Gatsby framework. It's a separate section to optimize SEO for
+          news. CMS is Contentful, where Gatsby download posts on every build.
+          Hosted on Netlify.
+        </P>
       </ProjectSection>
     </GlobalWrapper>
   );
