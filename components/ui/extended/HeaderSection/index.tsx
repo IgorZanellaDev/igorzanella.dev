8250deac65a6e8a1951ce7fbaa51c6748c935ecb
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-13 14:58:48 +0200

    feat: better style in HeaderSection

--- components/ui/extended/HeaderSection/index.tsx
+++ components/ui/extended/HeaderSection/index.tsx
@@ -4,9 +4,9 @@ import TechnologyStack from "./TechnologyStack";
 
 const HeaderSection: FunctionComponent = () => {
   return (
-    <section className="flex justify-between flex-col py-16 xl:flex-row xl:space-x-12 space-y-16 xl:space-y-0">
+    <section className="flex justify-between flex-col py-8 xl:flex-row xl:space-x-12 space-y-16 xl:space-y-0">
       <div className="flex justify-start items-start flex-col">
-        <div className="flex mb-1 justify-center items-center text-xl text-gray-400 py-1 px-3 bg-gray-800 rounded-full">
+        <div className="flex mb-1 justify-center items-center text-lg lg:text-xl text-gray-400 py-1 px-3 bg-gray-800 rounded-full">
           <Led color={"green"} className={"mr-2"} /> Available for new projects
         </div>
         <div>
@@ -15,12 +15,12 @@ const HeaderSection: FunctionComponent = () => {
           </h1>
         </div>
         <div className="md:mt-8 mt-4">
-          <h2 className="text-2xl leading-6 font-medium text-white">
+          <h2 className="text-2xl md:text-3xl leading-6 font-medium text-white">
             Full Stack Developer and DevOps specialist
           </h2>
         </div>
         <div className="md:mt-4 mt-2">
-          <p className="text-base leading-normal text-gray-400 xl:w-96">
+          <p className="text-base md:text-lg leading-normal text-gray-400 xl:w-96">
             I am a Freelance Full Stack Developer located in{" "}
             <a
               className="text-gray-200 underline"
