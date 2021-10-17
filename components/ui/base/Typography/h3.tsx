ae5c4eb614ab5d7015334383864eec08f3c1366f
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-18 01:24:48 +0200

    feat: change H3 font weight

--- components/ui/base/Typography/h3.tsx
+++ components/ui/base/Typography/h3.tsx
@@ -8,7 +8,7 @@ interface H3Props {
 const H3: FunctionComponent<H3Props> = (props: H3Props) => {
   return (
     <h3
-      className={`text-white text-xl lg:text-3xl font-semibold mt-2 ${props.className}`}
+      className={`text-white text-xl lg:text-3xl font-medium mt-2 ${props.className}`}
     >
       {props.children}
     </h3>
