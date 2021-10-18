1ac4b631304c99b6b2ec1b5d37bd45de5a356548
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-19 01:38:59 +0200

    feat: start Customerly page

--- pages/projects/customerly/index.tsx
+++ pages/projects/customerly/index.tsx
@@ -1,11 +1,55 @@
 import type { NextPage } from "next";
 import GlobalWrapper from "components/core/GlobalWrapper";
 import SEO from "components/core/SEO";
+import ProjectTitle from "components/ui/base/ProjectTitle";
 
 const Customerly: NextPage = () => {
   return (
     <GlobalWrapper withLayout>
-      <SEO title={"Racing Line Motorsport"} />
+      <SEO title={"Customerly"} />
+      <ProjectTitle
+        subtitle={"Freelance project"}
+        id={"customerly"}
+        links={{
+          web: "https://www.customerly.io",
+          facebook: "https://www.facebook.com/customerly/",
+          instagram: "https://www.instagram.com/customerly/",
+          twitter: "https://twitter.com/customerlyio",
+          linkedin: "https://www.linkedin.com/company/customerly/",
+        }}
+        name={"Customerly"}
+        category={"Communication Suite"}
+        imageWidth={1920}
+        imageHeight={1080}
+        technologies={[
+          "aws",
+          "chargify",
+          "circleci",
+          "contentful",
+          "css3",
+          "docker",
+          "ganalytics",
+          "gatsby",
+          "graphql",
+          "html5",
+          "i18next",
+          "javascript",
+          "laravel",
+          "mixpanel",
+          "php",
+          "react",
+          "react-query",
+          "recoil",
+          "redux",
+          "sass",
+          "svelte",
+          "sveltekit",
+          "symfony",
+          "terraform",
+          "typescript",
+          "wordpress",
+        ]}
+      />
     </GlobalWrapper>
   );
 };
