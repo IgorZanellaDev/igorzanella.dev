35356a156546be6b7108f21682c303215264c19f
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-24 16:38:53 +0200

    feat: add react-query and graphql-request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,28 @@ import "animate.css";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { GTM_ID } from "constants/seo";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import TagManager from "react-gtm-module";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 const tagManagerArgs = {
   gtmId: GTM_ID,
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
+
 export default MyApp;
