6ffd8e102f7b3e773f777e6b84fb7807badcdedb
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-27 00:41:44 +0200

    feat: optimize SEO

--- components/core/SEO/index.tsx
+++ components/core/SEO/index.tsx
@@ -1,40 +1,74 @@
 import { FunctionComponent } from "react";
 import Head from "next/head";
-import { OpenGraphObject, TwitterCard } from "types/seo";
 import { Thing, WithContext } from "schema-dts";
-import { TITLE_SUFFIX } from "constants/seo";
+import { DEFAULT_META_IMG_SRC, TITLE, TITLE_SUFFIX } from "constants/seo";
 import { useRouter } from "next/router";
-import { WEBSITE_URL } from "constants/url";
+import { TWITTER_ID, WEBSITE_URL } from "constants/url";
 
 interface SEOProps {
   title: string;
   withoutTitleSuffix?: boolean;
   withoutDefaultStructuredData?: boolean;
-  description?: string;
+  description: string;
   canonical?: string;
   image?: string;
-  openGraph?: OpenGraphObject;
-  twitterCard?: TwitterCard;
-  meta?: JSX.IntrinsicElements["meta"][];
+  imageAlt?: string;
   structuredData?: WithContext<Thing>[];
 }
 
 const SEO: FunctionComponent<SEOProps> = (props: SEOProps) => {
+  const canonical = WEBSITE_URL + useRouter().pathname;
+  const title = `${props.title} ${
+    !props.withoutTitleSuffix ? TITLE_SUFFIX : ""
+  }`;
+
+  const defaultStructuredData: WithContext<Thing>[] = [];
+
   return (
     <Head>
-      <title>
-        {props.title}
-        {!props.withoutTitleSuffix ? TITLE_SUFFIX : ""}
-      </title>
-      {props.description && (
-        <meta name="description" content={props.description} />
-      )}
+      <title>{title}</title>
+      <meta name="og:title" content={title} />
+      <meta name="twitter:title" content={title} />
+      <meta name="og:site_name" content={TITLE} />
+      <meta name="description" content={props.description} />
+      <meta name="og:description" content={props.description} />
+      <meta name="twitter:description" content={props.description} />
       <link
         rel="canonical"
-        href={
-          props.canonical ? props.canonical : WEBSITE_URL + useRouter().pathname
-        }
+        href={props.canonical ? props.canonical : canonical}
       />
+      <meta
+        name="og:url"
+        content={props.canonical ? props.canonical : canonical}
+      />
+      <meta
+        name="og:image"
+        content={props.image ? props.image : DEFAULT_META_IMG_SRC}
+      />
+      <meta
+        name="twitter:image"
+        content={props.image ? props.image : DEFAULT_META_IMG_SRC}
+      />
+      <meta
+        name="og:image:alt"
+        content={props.imageAlt ? props.imageAlt : TITLE}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={TWITTER_ID} />
+      {props.structuredData &&
+        props.structuredData.map((data, i) => (
+          <script type="application/ld+json" key={i}>
+            {JSON.stringify(data)}
+          </script>
+        ))}
+      {!props.withoutDefaultStructuredData &&
+        defaultStructuredData.map((data, i) => {
+          return (
+            <script type="application/ld+json" key={i}>
+              {JSON.stringify(data)}
+            </script>
+          );
+        })}
       <link
         rel="apple-touch-icon"
         sizes="180x180"
--- constants/seo.ts
+++ constants/seo.ts
@@ -1,3 +1,4 @@
 export const TITLE = "igorzanella.dev";
 export const TITLE_SUFFIX = " | " + TITLE;
 export const GTM_ID = "GTM-5MW4L2G";
+export const DEFAULT_META_IMG_SRC = "/meta.jpg";
