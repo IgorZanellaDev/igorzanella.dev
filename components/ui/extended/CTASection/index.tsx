0797d275568cfee384e22a3b31444bca638d968f
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-27 00:01:27 +0200

    fix: fix CTASection

--- components/ui/extended/CTASection/index.tsx
+++ components/ui/extended/CTASection/index.tsx
@@ -10,22 +10,22 @@ const CTASection: FunctionComponent = () => {
           type={"fadeInDown"}
           className="flex justify-center items-center flex-col"
         >
-          <h2 className="text-3xl md:text-6xl text-white font-extrabold">
-            Do you have an idea? 💡
+          <h2 className="text-4xl text-center md:text-6xl text-white font-extrabold">
+            Do you have an idea?
           </h2>
-          <h3 className="mt-2 text-2xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-iz-yellow-light to-iz-yellow-dark">
+          <h3 className="mt-2 text-3xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-iz-yellow-light to-iz-yellow-dark">
             Let's talk about it!
           </h3>
         </Animation>
-        <div className="flex justify-center items-center mt-8">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8">
           <Animation type={"fadeInLeft"}>
-            <button className="mx-2 py-6 px-8 rounded-xl text-xl md:text-2xl font-bold text-white bg-gradient-to-b from-iz-blue-light to-iz-blue-dark hover:to-iz-blue-dark-darker-10">
+            <button className="flex m-2 py-6 px-8 rounded-xl text-xl md:text-2xl font-bold text-white bg-gradient-to-b from-iz-blue-light to-iz-blue-dark hover:to-iz-blue-dark-darker-10">
               Contact me
             </button>
           </Animation>
           <Animation type={"fadeInRight"}>
             <Link href={"/projects"}>
-              <a className="mx-2 py-6 px-8 rounded-xl text-xl md:text-2xl font-bold text-white bg-gradient-to-b from-gray-700 to-gray-800">
+              <a className="flex m-2 py-6 px-8 rounded-xl text-xl md:text-2xl font-bold text-white bg-gradient-to-b from-gray-700 to-gray-800">
                 Go to projects
               </a>
             </Link>
