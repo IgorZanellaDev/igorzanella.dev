d2ba409c5462304d57dda87c59751c7079848211
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-27 11:10:56 +0200

    fix: fix default meta img url

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,4 +1,6 @@
+import { WEBSITE_URL } from "constants/url";
+
 export const TITLE = "igorzanella.dev";
 export const TITLE_SUFFIX = " | " + TITLE;
 export const GTM_ID = "GTM-5MW4L2G";
-export const DEFAULT_META_IMG_SRC = "/meta.jpg";
+export const DEFAULT_META_IMG_SRC = WEBSITE_URL + "/meta.jpg";
