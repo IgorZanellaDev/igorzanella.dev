a5a2c35e7269c0059fbc2d031cfd94e3c93629f3
Igor Zanella <igorzanella.dev@gmail.com>
2021-10-27 11:09:51 +0200

    fix: fix ProjectsSection use component name

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import GlobalWrapper from "../../components/core/GlobalWrapper";
 import SEO from "../../components/core/SEO";
 import TitleSection from "components/ui/base/TitleSection";
-import ProjectSection from "components/ui/extended/ProjectsSection";
+import ProjectsSection from "components/ui/extended/ProjectsSection";
 
 const Projects: NextPage = () => {
   return (
@@ -18,7 +18,7 @@ const Projects: NextPage = () => {
           "Here you can find some of my projects or projects in which I have worked, if you are interested in something else contact me, i did not post them all."
         }
       />
-      <ProjectSection />
+      <ProjectsSection />
     </GlobalWrapper>
   );
 };
