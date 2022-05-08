9290b978662581d68da89567b4c54032bc37d30a
Igor Zanella <igorzanella.dev@gmail.com>
2022-05-08 14:12:19 +0200

    feat: start Courses section

---
+++ components/ui/base/Course/index.tsx
@@ -0,0 +1,63 @@
+import React, { FunctionComponent } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Animation from "components/core/Animation";
+
+interface CourseProps {
+  id: string;
+  description: string;
+  inverted?: boolean;
+  imageWidth: number;
+  imageHeight: number;
+  title: string;
+  type: string;
+}
+
+const Course: FunctionComponent<CourseProps> = (props: CourseProps) => {
+  return (
+    <div
+      className={`flex py-8 flex-col ${
+        props.inverted ? "lg:flex-row-reverse" : "lg:flex-row"
+      }`}
+    >
+      <Animation
+        type={"fadeInUp"}
+        className="flex w-full lg:w-3/5 relative lg:p-10"
+      >
+        <Link href={`/courses/${props.id}`}>
+          <a className="overflow-hidden rounded-lg md:rounded-xl lg:rounded-2xl">
+            <Image
+              src={`/course/previews/${props.id}.jpg`}
+              width={props.imageWidth}
+              height={props.imageHeight}
+              className="overflow-hidden rounded-lg md:rounded-xl lg:rounded-2xl"
+              placeholder="blur"
+              blurDataURL={`/course/previews/${props.id}.jpg`}
+              alt={props.title}
+            />
+          </a>
+        </Link>
+      </Animation>
+      <Animation
+        type={"fadeInUp"}
+        className="flex flex-col text-white justify-center mt-2 lg:mt-0 flex-1"
+      >
+        <p className="text-base font-semibold text-iz-blue-light tracking-wide uppercase">
+          {props.type}
+        </p>
+        <Link href={`/course/${props.id}`}>
+          <a>
+            <h3 className="text-3xl lg:text-5xl font-bold transition hover:border-b-8 border-white">
+              {props.title}
+            </h3>
+          </a>
+        </Link>
+        <p className="mt-2 text-xl text-gray-500 flex flex-wrap max-w-lg">
+          {props.description}
+        </p>
+      </Animation>
+    </div>
+  );
+};
+
+export default Course;
---
+++ components/ui/extended/CoursesSection/index.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from "react";
+import Project from "components/ui/base/Project";
+import Course from "components/ui/base/Course";
+
+const CoursesSection: FunctionComponent = () => {
+  return (
+    <section>
+      <Course
+        id={"bestondesk"}
+        type={"React"}
+        title={"React, Typescript e Styled Components: da zero a webapp!"}
+        description={
+          "Italian blog, business created to earn with affiliate marketing, Amazon and others."
+        }
+        imageWidth={1920}
+        imageHeight={1080}
+      />
+    </section>
+  );
+};
+
+export default CoursesSection;
