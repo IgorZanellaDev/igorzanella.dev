e33d7ff25b0c124a48b3830b1db27e7e24867486
Igor Zanella <igorzanella.dev@gmail.com>
2023-06-02 12:14:24 +0100

    feat: invert courses order

--- components/ui/extended/CoursesSection/index.tsx
+++ components/ui/extended/CoursesSection/index.tsx
@@ -5,6 +5,18 @@ const CoursesSection: FunctionComponent = () => {
   return (
     <section>
       <Course
+        id={"mrl"}
+        language={"🇺🇸 English"}
+        title={"(-30%) Master React Libraries (with Customerly codebase)"}
+        description={
+          "Learn the most used React libraries: React Router, Tanstack Query, Recoil and i18next with Customerly production codebase!"
+        }
+        link={"https://go.igorzanella.dev/courses/mrl-izdev"}
+        imageWidth={1920}
+        imageHeight={1080}
+      />
+      <Course
+        inverted
         id={"react-ts-sc"}
         language={"🇮🇹 Italian"}
         title={
@@ -14,18 +26,6 @@ const CoursesSection: FunctionComponent = () => {
           "Learn React basics, using Typescript to write better code, and using Styled Components to style your components."
         }
         link={"https://go.igorzanella.dev/courses/react-ts-sc-discounted"}
-        imageWidth={1920}
-        imageHeight={1080}
-      />
-      <Course
-        inverted
-        id={"mrl"}
-        language={"🇺🇸 English"}
-        title={"(-30%) Master React Libraries (with Customerly codebase)"}
-        description={
-          "Learn the most used React libraries: React Router, Tanstack Query, Recoil and i18next with Customerly production codebase!"
-        }
-        link={"https://go.igorzanella.dev/courses/mrl-izdev"}
         imageWidth={1920}
         imageHeight={1080}
       />
