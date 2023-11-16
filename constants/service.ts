5adab8fe2685112ccfe41ee28ffe1b4e3a6c12d2
Igor Zanella <igorzanella.dev@gmail.com>
2023-11-17 00:28:11 +0100

    fix: fix services in home page

--- constants/service.ts
+++ constants/service.ts
@@ -4,7 +4,7 @@ export const SERVICES: Service[] = [
   {
     name: "Applications",
     description:
-      "Webapps, mobile apps, frontend UI/UX design, features development",
+      "Web apps, mobile apps, frontend UI/UX design, features development",
     icon: "display-grid",
   },
   {
@@ -14,26 +14,8 @@ export const SERVICES: Service[] = [
     icon: "database",
   },
   {
-    name: "Server support",
-    description: "Provide, install and configure servers. DevOps tasks.",
+    name: "DevOps",
+    description: "Provide, install and configure servers, CI/CD. DevOps tasks.",
     icon: "terminal",
-  },
-  {
-    name: "Websites and ecommerces",
-    description:
-      "Jamstack websites, with headless CMS. But also on Wordpress, Shopify, etc.",
-    icon: "globe",
-  },
-  {
-    name: "Tests and security reports",
-    description:
-      "Apps and services security tests, with detailed reports for problems.",
-    icon: "play",
-  },
-  {
-    name: "SEO",
-    description:
-      "SEO analysis, reports, optimization and positioning of your website.",
-    icon: "google",
   },
 ];
