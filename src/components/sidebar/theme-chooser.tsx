2c4ec7949c24efd627783ce6896256798952a9df
Igor Zanella <igorzanella.dev@gmail.com>
2025-03-01 18:33:27 +0100

    feat: add tooltip to ThemeChooser

--- src/components/sidebar/theme-chooser.tsx
+++ src/components/sidebar/theme-chooser.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
 import { FC, ReactNode, useEffect, useState } from "react";
@@ -21,19 +22,28 @@ const ThemeButton: FC<{ children: ReactNode; theme: string }> = ({ children, the
   const [currentTheme, setTheme] = useThemeSwitcher();
 
   return (
-    <Button
-      className={cn("w-full", currentTheme !== theme && "border border-transparent")}
-      onClick={() => setTheme(theme)}
-      variant={currentTheme === theme ? "outline" : "ghost"}
-    >
-      {children}
-    </Button>
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <Button
+            className={cn("w-full", currentTheme !== theme && "border border-transparent")}
+            onClick={() => setTheme(theme)}
+            variant={currentTheme === theme ? "outline" : "ghost"}
+          >
+            {children}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{theme.charAt(0).toUpperCase() + theme.slice(1)}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
 const ThemeChooser: FC = () => {
   return (
-    <div className={"flex gap-1 p-2"}>
+    <div className={"flex gap-1"}>
       <ThemeButton theme={"system"}>
         <LuMonitor />
       </ThemeButton>
